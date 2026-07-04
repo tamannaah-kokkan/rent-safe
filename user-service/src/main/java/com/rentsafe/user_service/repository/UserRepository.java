3a99d8198912e3bc709599e32e5f45ae3c017993
tamannaah-kokkan <tamannaah1302@gmail.com>
2026-07-04 23:29:34 +0530

    created the user repository

--- user-service/src/main/java/com/rentsafe/user_service/repository/UserRepository.java
+++ user-service/src/main/java/com/rentsafe/user_service/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.rentsafe.user_service.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.rentsafe.user_service.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	public User findByEmail(String email);
 }
